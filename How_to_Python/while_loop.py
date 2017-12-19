141bcb6aa20fd81fe3a43a2350070ec6f15c4814
Juzer Shakir <juz.shakir5253@gmail.com>
2017-12-19 16:11:35 +0530

    Using loops in Python.
    A demonstration on how to use while loop in Python.

---
+++ How_to_Python/while_loop.py
@@ -0,0 +1,20 @@
+""" Author : Juzer Shakir   ||  Created on 10 Nov 2017     ||   Last modified on 18 Dec 2017 1:15pm
+Topic : How to while-loop in python"""
+"""----------------------------------------------------------------------------------------------------------------------------------------------------------"""
+
+#while loop checks the condition, if true will run a block of code until it's condition is falsed
+#it circles itself
+
+#let's define a variable and give a value
+x = 1
+#while loop will check the conditions
+while x <= 5:
+#this block of code will execute if conditions are true
+    print("I am learning python! One day i will be the greatest python developer!")
+#in order to prevent from looping forever, we need to increament the value by 1 on each execution
+    x += 1
+#the while loop function will stop as soon as the conditions turns false
+
+
+#That's it!
+print("\nThe END!")
