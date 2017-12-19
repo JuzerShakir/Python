6f20461987e1ebaf27533fb79e8405b5a1f1dfda
Juzer Shakir <juz.shakir5253@gmail.com>
2017-12-19 15:40:16 +0530

    Making a Histogram in Python
    A demonstration on how to make a histogram using Matplotlib mobule in
    Python

---
+++ How_to_Python/Modules/Matplotlib/Histogram.py
@@ -0,0 +1,27 @@
+""" Author : Juzer Shakir   ||  Created on 10 Nov 2017     ||   Last modified on 19 Dec 2017 9:18am
+Topic : How to use matplotlib module in python (using Histogram)
+Result : Results shown in this directory named 'histogram.png'"""
+"""----------------------------------------------------------------------------------------------------------------------------------------------------------"""
+
+
+#importing module
+from matplotlib import pyplot as plt
+
+#'quantities' varaible contains list of numbers measured on y-axis
+quantities = [22, 44, 35, 23, 20, 32, 36, 41, 37, 22, 27]
+#(x-axis) 'bins' varaible contains list of numbers with Bin size of five in which 'quantities' values will be plotted
+bins = [20, 25, 30, 35, 40, 45]
+#plotting histogram by giving 'y' value first
+plt.hist(quantities, bins, histtype = 'bar', rwidth = 0.9)
+
+#title of graph
+plt.title("Histogram")
+#labeling x-axis
+plt.xlabel("Age")
+#labeling y-axis
+plt.ylabel("No. of Adults")
+#displaying graph
+plt.show()
+
+
+#That's it!
