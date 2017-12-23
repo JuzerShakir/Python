80f78a9e6f429f89128a2bef9d85ee09dad630ad
Juzer Shakir <juz.shakir5253@gmail.com>
2017-12-23 15:35:13 +0530

    Optimized code for better readability.

--- How_to_Python/Modules/Matplotlib/Bar.py
+++ How_to_Python/Modules/Matplotlib/Bar.py
@@ -3,24 +3,24 @@ Topic : How to use matplotlib module in python (using Bar Graph)
 Result : Results shown in this directory named 'bar.png'"""
 """----------------------------------------------------------------------------------------------------------------------------------------------------------"""
 
-#importing module
+# importing module
 from matplotlib import pyplot as plt
 
-#plotting bars of two topics, cars and bikes
-#giving two lists where first list contains values of 'x' and second of 'y'
-plt.bar([1, 3, 5], [2, 6, 4], color = 'g', label = "cars", width = 0.1)
-plt.bar([2, 4, 6], [10, 5, 3], color = 'b', label = "bikes", width = 0.1)
+# plotting bars of two topics, cars and bikes
+# giving two lists where first list contains values of 'x' and second of 'y'
+plt.bar([1, 3, 5], [2, 6, 4], color='g', label="cars", width=0.1)
+plt.bar([2, 4, 6], [10, 5, 3], color='b', label="bikes", width=0.1)
 
-#title of graph
+# title of graph
 plt.title("Cars vs Bikes")
-#displays legend on the graph
+# displays legend on the graph
 plt.legend()
-#labeling x-axis
+# labeling x-axis
 plt.xlabel("days")
-#labeling y-axis
+# labeling y-axis
 plt.ylabel("Accidents")
-#displaying graph
+# displaying graph
 plt.show()
 
 
-#That's it!
+# That's it!
--- How_to_Python/Modules/Matplotlib/Histogram.py
+++ How_to_Python/Modules/Matplotlib/Histogram.py
@@ -4,24 +4,24 @@ Result : Results shown in this directory named 'histogram.png'"""
 """----------------------------------------------------------------------------------------------------------------------------------------------------------"""
 
 
-#importing module
+# importing module
 from matplotlib import pyplot as plt
 
 #'quantities' varaible contains list of numbers measured on y-axis
 quantities = [22, 44, 35, 23, 20, 32, 36, 41, 37, 22, 27]
 #(x-axis) 'bins' varaible contains list of numbers with Bin size of five in which 'quantities' values will be plotted
 bins = [20, 25, 30, 35, 40, 45]
-#plotting histogram by giving 'y' value first
-plt.hist(quantities, bins, histtype = 'bar', rwidth = 0.9)
+# plotting histogram by giving 'y' value first
+plt.hist(quantities, bins, histtype='bar', rwidth=0.9)
 
-#title of graph
+# title of graph
 plt.title("Histogram")
-#labeling x-axis
+# labeling x-axis
 plt.xlabel("Age")
-#labeling y-axis
+# labeling y-axis
 plt.ylabel("No. of Adults")
-#displaying graph
+# displaying graph
 plt.show()
 
 
-#That's it!
+# That's it!
